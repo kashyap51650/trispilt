d88440c261bf8bc68a9699b67203437982ea6b2a
Kashyap Patel <kashyap.p@simformsolutions.com>
2025-08-23 10:37:40 +0530

    refactor: update PageHeader layout to improve back navigation and title alignment

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -22,14 +22,14 @@ const PageHeader: React.FC<PageHeaderProps> = ({
   };
 
   return (
-    <header className="grid grid-cols-3 items-center gap-4 w-full">
+    <header className="grid grid-cols-5 items-center gap-4 w-full">
       <div
-        className="bg-primary/10 rounded-full p-1 text-primary cursor-pointer hover:bg-primary/20 transition w-fit"
+        className="col-span-1 bg-primary/10 rounded-full p-1 text-primary cursor-pointer hover:bg-primary/20 transition w-fit text-center"
         onClick={backNavigation}
       >
         <ChevronLeft />
       </div>
-      <div>
+      <div className="col-span-3 text-center">
         <h1>{title}</h1>
         {description && (
           <p className="text-sm text-muted-foreground">{description}</p>
