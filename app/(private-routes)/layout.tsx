0528788a47574b3ea918d0fab34d2151de71f14e
Kashyap Patel <kashyap.p@simformsolutions.com>
2025-08-28 12:08:57 +0530

    feat: add React Query integration with provider and update layout structure

--- app/(private-routes)/layout.tsx
+++ app/(private-routes)/layout.tsx
@@ -1,19 +1,22 @@
 import NavigationFooter from "@/components/NavigationFooter";
+import ReactQueryProvider from "@/provider/ReactQueryProvider";
 import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-full flex flex-col-reverse">
-      <NavigationFooter />
-      <div className="flex-1 relative p-4">
-        <div className="pointer-events-none absolute inset-0 -z-10 h-full overflow-hidden">
-          <div className="absolute top-0 left-1/4 h-60 w-60 rounded-full bg-primary/25 blur-3xl" />
-          <div className="absolute bottom-10 right-1/3 h-72 w-72 rounded-full bg-secondary/25 blur-2xl" />
-          <div className="absolute top-1/2 left-1/2 h-40 w-40 -translate-x-1/2 -translate-y-1/2 rounded-full bg-accent/20 blur-2xl" />
-          <div className="absolute inset-0 bg-gradient-to-br from-background via-card/30 to-transparent" />
+      <ReactQueryProvider>
+        <NavigationFooter />
+        <div className="flex-1 relative p-4">
+          <div className="pointer-events-none absolute inset-0 -z-10 h-full overflow-hidden">
+            <div className="absolute top-0 left-1/4 h-60 w-60 rounded-full bg-primary/25 blur-3xl" />
+            <div className="absolute bottom-10 right-1/3 h-72 w-72 rounded-full bg-secondary/25 blur-2xl" />
+            <div className="absolute top-1/2 left-1/2 h-40 w-40 -translate-x-1/2 -translate-y-1/2 rounded-full bg-accent/20 blur-2xl" />
+            <div className="absolute inset-0 bg-gradient-to-br from-background via-card/30 to-transparent" />
+          </div>
+          <div className="h-full overflow-y-auto">{children}</div>
         </div>
-        <div className="h-full overflow-y-auto">{children}</div>
-      </div>
+      </ReactQueryProvider>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import SplashPage from "./(public-routes)/splash/page";
 
 export default function Home() {
-  return (
-    <div>
-      <SplashPage />
-    </div>
-  );
+  return <SplashPage />;
 }
